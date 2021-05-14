8d2c3b701ef95a50168609aa71889f740ee9c0ea
Carlos Jiménez <carlosjimenez@xcjgames.com>
2021-05-14 16:40:33 +0200

    Door mechanics

---
+++ Assets/Scripts/Door.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Door : Interactable
+{
+    public Door destinationDoor;
+
+    public float destinationOffset = 1f;
+
+    public override void Interact()
+    {
+        Debug.Log("interacting with door");
+        Vector2 newPos = destinationDoor.transform.position;
+        newPos.y += destinationOffset;
+
+        player.transform.position = newPos;
+    }
+}
