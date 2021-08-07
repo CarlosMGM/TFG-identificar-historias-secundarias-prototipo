ec9cb54cad43e836610c8e60adc2ac41c3617612
Carlos Gonzalez <gonzalem@ucm.es>
2021-08-07 12:22:33 +0200

    fixed never ending quest

--- Assets/Scripts/Managers/QuestManager.cs
+++ Assets/Scripts/Managers/QuestManager.cs
@@ -134,7 +134,10 @@ public class QuestManager
             QuestStarterNPC npc = character.GetComponent<QuestStarterNPC>();
             if (npc == null) 
                 npc = character.AddComponent<QuestStarterNPC>();
-            npc.quest = scene.quest;
+            
+            if(npc != previousNPC)
+                GameObject.Destroy(previousNPC);
+                npc.quest = scene.quest;
             npc.itemToGive = scene.itemToGive;
             npc.enabled = true;
             npc.place = place;
@@ -145,6 +148,10 @@ public class QuestManager
             QuestFinisherNPC npc = character.GetComponent<QuestFinisherNPC>();;
             if (npc == null) 
                 npc = character.AddComponent<QuestFinisherNPC>();
+            
+            if(npc != previousNPC)
+                GameObject.Destroy(previousNPC);
+            
             npc.quest = scene.quest;
             npc.itemToGive = scene.itemToGive;
             npc.itemToTake = scene.itemToTake;
